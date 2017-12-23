ce5488d7f2c09ec01a5f05270a6a8fb0555141db
Morrie <morriedog@hotmail.com>
2017-12-23 20:18:47 +0800

    ok?

---
+++ RebuildingRails/rulers/lib/rulers.rb
@@ -0,0 +1,11 @@
+require "rulers/version"
+require "rulers/array"
+
+module Rulers
+  class Application
+    def call(env)
+      `echo debug > debug.text`;
+      [200, {'Content-Type' => 'text/html'}, ["hello, I'm from rulers #{[1,2,3].sum} "]]
+    end
+  end
+end
